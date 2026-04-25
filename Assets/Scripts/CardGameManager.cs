aec43f18afbe625ed4212e352615b79b4537035a
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-04-25 23:44:35 +0900

    コミット＆プッシュ

---
+++ Assets/Scripts/CardGameManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem.Switch;
+
+public class CardGameManager : MonoBehaviour
+{
+    
+    [SerializeField] GameObject card;
+    [SerializeField] Sprite[] sprite;
+    SpriteRenderer spriteRender;
+
+    int ran;
+    int count;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ran = Random.Range(0, 2);
+
+        spriteRender = card.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ran == 0)
+        {
+        }
+        else
+        {
+        }
+        if (count == 0)
+        {
+            Instantiate(card);
+            count++;
+        }
+        switch (ran)
+        {
+            case 0:
+                spriteRender.sprite = sprite[0];
+                break;
+            case 1:
+                spriteRender.sprite = sprite[1];
+                break;
+            case 2:
+                spriteRender.sprite = sprite[2];
+                break;
+            case 3:
+                spriteRender.sprite = sprite[3];
+                break;
+            case 4:
+                 spriteRender.sprite = sprite[4];
+                break;
+            case 5:
+                spriteRender.sprite = sprite[5];
+                break;
+            case 6:
+                spriteRender.sprite = sprite[6];
+                break;
+            case 7:
+                spriteRender.sprite = sprite[7];
+                break;
+            case 8:
+                spriteRender.sprite = sprite[8];
+                break;
+            case 9:
+                spriteRender.sprite = sprite[9];
+                break;
+            case 10:
+                spriteRender.sprite = sprite[10];
+                break;
+            case 11:
+                spriteRender.sprite = sprite[11];
+                break;
+        }
+    }
+}
